8f8d88f1919ab54527afc787aff365f4a2082e22
Samuel Adeyemi <Samuel3adeyemi@live.com>
2024-10-02 00:06:00 +1000

    Responsiveness Issues Fixed

--- components/projectCard.jsx
+++ components/projectCard.jsx
@@ -23,27 +23,28 @@ const ProjectCard = ({ link, title, role, image, isVideo }) => {
 				onMouseEnter={() => setHover(true)}
 				onMouseLeave={() => setHover(false)}
 				id='portfolio'
-				className='2xl:w-[900px] xl:w-[700px] w-full 2xl:h-[510px] lg:h-[380px] shrink-0 my-1 bg-[#161616] lg:rounded-[12px] overflow-hidden'
+				className='w-full max-w-[900px] lg:max-w-[700px] sm:max-w-[500px] h-auto lg:h-[380px] shrink-0 my-1 bg-[#161616] lg:rounded-[12px] overflow-hidden relative'
 			>
 				<motion.div
-					variants={hover === true ? open : closed}
+					variants={hover ? open : closed}
 					animate='default'
 					transition={{ ease: "easeOut", duration: 0.5 }}
-					className='2xl:w-[900px] xl:w-[700px] w-[200px] 2xl:h-[510px] lg:h-[380px] rounded-[12px] flex-col backdrop-blur-sm hidden lg:flex justify-center items-center absolute bg-black/50'
+					className='w-full h-full rounded-[12px] flex-col backdrop-blur-sm hidden lg:flex justify-center items-center absolute bg-black/50'
 				>
-					<h1 className='text-[72px] font-["shallota"] leading-[40px]'>{title}</h1>
-					<p className='font-["helvetica-extended"] font-[400] '>{role}</p>
+					<h1 className='text-[4vw] lg:text-[72px] font-["shallota"] leading-[1.2]'>{title}</h1>
+					<p className='font-["helvetica-extended"] font-[400]'>{role}</p>
 				</motion.div>
 
 				{isVideo ? (
-					<video className='object-fill object-center h-auto w-full' autoPlay={true} loop={true} muted={true} src={image} controls={false} />
+					<video className='object-cover object-center h-full w-full' autoPlay loop muted src={image} controls={false} />
 				) : (
-					<Image alt={title} className='object-fill object-center h-auto w-full' src={image} width={600} height={317} />
+					<Image alt={title} className='object-cover object-center h-full w-full' src={image} width={600} height={317} />
 				)}
 			</Link>
-			<div className='lg:hidden flex flex-col gap-0 items-center'>
-				<h1 className='text-[36px] font-["shallota"] mt-2 mb-[-10px]'>{title}</h1>
-				<p className='text-[14px] font-["helvetica-extended"] font-[400] '>{role}</p>
+
+			<div className='lg:hidden flex flex-col items-center mt-2'>
+				<h1 className='text-[8vw] sm:text-[36px] font-["shallota"]'>{title}</h1>
+				<p className='text-[3vw] sm:text-[14px] font-["helvetica-extended"] font-[400]'>{role}</p>
 			</div>
 		</div>
 	);
